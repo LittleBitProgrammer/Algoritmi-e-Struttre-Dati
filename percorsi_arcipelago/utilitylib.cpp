272feff0b7d7afcee4764899f96afae9f893404c
Roberto <vecchioroberto93@gmail.com>
2021-11-09 16:11:23 +0100

    Max Heap
    - Aggiunta struttura dati Max Heap

---
+++ percorsi_arcipelago/utilitylib.cpp
@@ -0,0 +1,8 @@
+#include "utilitylib.h"
+
+void swap(int& a, int& b)
+{
+    int temp{a};
+    a = b;
+    b = temp;
+}
---
+++ percorsi_arcipelago/utilitylib.h
@@ -0,0 +1,6 @@
+#ifndef _UTILITY_LIB_H_
+#define _UTILITY_LIB_H_
+
+void swap(int&,int&);
+
+#endif
