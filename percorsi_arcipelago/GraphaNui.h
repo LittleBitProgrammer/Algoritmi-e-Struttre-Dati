0c74f5becbf90bf71108710981374be4f4b295f5
Roberto <vecchioroberto93@gmail.com>
2021-11-12 09:42:53 +0100

    fix
    - fix minori

---
+++ percorsi_arcipelago/GraphaNui.h
@@ -0,0 +1,15 @@
+#ifndef _GRAPHANUI_H_
+#define _GRAPHANUI_H_
+
+#include "Graph.h"
+
+/* Signleton class*/
+class GraphaNui
+{
+    private:
+        //Graph archipelago_grapha_nui;
+
+    public:
+};
+
+#endif
